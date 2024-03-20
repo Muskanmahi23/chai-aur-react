2c4287bb60ec195cba0d86dd409557eda86dd053
Muskanmahi23 <93921249+Muskanmahi23@users.noreply.github.com>
2024-03-20 16:01:14 +0530

    basic react creation

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,23 @@
+
+import Chai from "./chai"
+
+function App() {
+
+
+  return (
+    // <div>
+    //   <Chai/>
+    //   <h1>Chai aur react</h1>
+    //   <p>test para</p>
+    // </div>
+
+    <>
+      <Chai/>
+      <h1>chai aur react</h1>
+      <p>test para</p>
+    </>
+    
+  )
+}
+
+export default App
